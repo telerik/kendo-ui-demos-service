04e2da994e7cf4fffac2f91f9dfc85afee957fcf
Ianko Djemerenov <iankodj@gmail.com>
2019-11-21 16:01:44 +0200

    chore: proto file manager service

---
+++ demos-and-odata-v3/KendoCRUDService/Controllers/FileManagerController.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KendoCRUDService.Models;
+using KendoCRUDService.Models.FileManager;
+
+namespace KendoCRUDService.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        private const string contentFolderRoot = "~/Content/";
+        private const string prettyName = "Files/";
+        private static readonly string[] foldersToCopy = new[] { "~/Content/editor/" };
+        private const string DefaultFilter = "*.txt,*.doc,*.docx,*.xls,*.xlsx,*.ppt,*.pptx,*.zip,*.rar,*.jpg,*.jpeg,*.gif,*.png";
+
+        private readonly DirectoryProvider directoryProvider;
+        private readonly ContentInitializer contentInitializer;
+
+        public FileManagerController()
+        {
+            directoryProvider = new DirectoryProvider();
+            contentInitializer = new ContentInitializer(contentFolderRoot, foldersToCopy, prettyName);
+        }
+
+        public string ContentPath
+        {
+            get
+            {
+                return contentInitializer.CreateUserFolder(Server);
+            }
+        }
+
+        private string ToAbsolute(string virtualPath)
+        {
+            return VirtualPathUtility.ToAbsolute(virtualPath);
+        }
+
+        private string CombinePaths(string basePath, string relativePath)
+        {
+            return VirtualPathUtility.Combine(VirtualPathUtility.AppendTrailingSlash(basePath), relativePath);
+        }
+
+        public virtual bool AuthorizeRead(string path)
+        {
+            return CanAccess(path);
+        }
+
+        protected virtual bool CanAccess(string path)
+        {
+            return path.StartsWith(ToAbsolute(ContentPath), StringComparison.OrdinalIgnoreCase);
+        }
+
+        private string NormalizePath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return ToAbsolute(ContentPath);
+            }
+
+            return CombinePaths(ToAbsolute(ContentPath), path);
+        }
+
+        public virtual JsonResult Read(string path)
+        {
+            path = NormalizePath(path);
+
+            if (AuthorizeRead(path))
+            {
+                try
+                {
+                    directoryProvider.Server = Server;
+
+                    var result = directoryProvider
+                        .GetContent(path, DefaultFilter)
+                        .Select(f => new
+                        {
+                            name = f.Name,
+                            size = f.Size,
+                            path = f.Path,
+                            extension = f.Extension,
+                            isDirectory = f.IsDirectory,
+                            hasDirectories = f.HasDirectories,
+                            created = f.Created,
+                            createdUtc = f.CreatedUtc,
+                            modified = f.Modified,
+                            modifiedUtc = f.ModifiedUtc
+                        });
+
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    throw new HttpException(404, "File Not Found");
+                }
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+        public virtual JsonResult DirectoryTreeRead(string path)
+        {
+            path = NormalizePath(path);
+
+            if (AuthorizeRead(path))
+            {
+                try
+                {
+                    directoryProvider.Server = Server;
+
+                    var result = directoryProvider
+                        .GetDirectoryTree(path)
+                        .Select(f => new
+                        {
+                            name = f.Name,
+                            size = f.Size,
+                            path = f.Path,
+                            extension = f.Extension,
+                            isDirectory = f.IsDirectory,
+                            hasDirectories = f.HasDirectories,
+                            created = f.Created,
+                            createdUtc = f.CreatedUtc,
+                            modified = f.Modified,
+                            modifiedUtc = f.ModifiedUtc,
+                            directories = f.Directories
+                        });
+
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    throw new HttpException(404, "File Not Found");
+                }
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public virtual ActionResult Destroy(string path, bool isDirectory)
+        {
+            path = NormalizePath(path);
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                if (isDirectory)
+                {
+                    DeleteDirectory(path);
+                }
+                else
+                {
+                    DeleteFile(path);
+                }
+
+                return Json(new object[0]);
+            }
+            throw new HttpException(404, "File Not Found");
+        }
+
+        public virtual bool AuthorizeDeleteFile(string path)
+        {
+            return CanAccess(path);
+        }
+
+        public virtual bool AuthorizeDeleteDirectory(string path)
+        {
+            return CanAccess(path);
+        }
+
+        protected virtual void DeleteFile(string path)
+        {
+            if (!AuthorizeDeleteFile(path))
+            {
+                throw new HttpException(403, "Forbidden");
+            }
+
+            var physicalPath = Server.MapPath(path);
+
+            if (System.IO.File.Exists(physicalPath))
+            {
+                System.IO.File.Delete(physicalPath);
+            }
+        }
+
+        protected virtual void DeleteDirectory(string path)
+        {
+            if (!AuthorizeDeleteDirectory(path))
+            {
+                throw new HttpException(403, "Forbidden");
+            }
+
+            var physicalPath = Server.MapPath(path);
+
+            if (Directory.Exists(physicalPath))
+            {
+                Directory.Delete(physicalPath, true);
+            }
+        }
+
+        public virtual bool AuthorizeCreateDirectory(string path, string name)
+        {
+            return CanAccess(path);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public virtual ActionResult Create(string path, FileBrowserEntry entry)
+        {
+            path = NormalizePath(path);
+            var name = entry.Name;
+
+            if (!string.IsNullOrEmpty(name) && AuthorizeCreateDirectory(path, name))
+            {
+                var physicalPath = Path.Combine(Server.MapPath(path), name);
+
+                if (!Directory.Exists(physicalPath))
+                {
+                    Directory.CreateDirectory(physicalPath);
+                }
+
+                return Json(new
+                        {
+                            name = entry.Name,
+                            size = entry.Size,
+                            isDirectory = true
+                        });
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+
+        public virtual bool AuthorizeUpload(string path, HttpPostedFileBase file)
+        {
+            return CanAccess(path) && IsValidFile(file.FileName);
+        }
+
+        private bool IsValidFile(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            var allowedExtensions = DefaultFilter.Split(',');
+
+            return allowedExtensions.Any(e => e.EndsWith(extension, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public virtual ActionResult Upload(string path, HttpPostedFileBase file)
+        {
+            path = NormalizePath(path);
+            var fileName = Path.GetFileName(file.FileName);
+
+            if (AuthorizeUpload(path, file))
+            {
+                file.SaveAs(Path.Combine(Server.MapPath(path), fileName));
+
+                return Json(new
+                {
+                    size = file.ContentLength,
+                    name = fileName,
+                    type = "f"
+                }, "text/plain");
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+        [OutputCache(Duration = 360, VaryByParam = "path")]
+        public ActionResult File(string fileName)
+        {
+            var path = NormalizePath(fileName);
+
+            if (AuthorizeFile(path))
+            {
+                var physicalPath = Server.MapPath(path);
+
+                if (System.IO.File.Exists(physicalPath))
+                {
+                    const string contentType = "application/octet-stream";
+                    return File(System.IO.File.OpenRead(physicalPath), contentType, fileName);
+                }
+            }
+
+            throw new HttpException(403, "Forbidden");
+        }
+
+        public virtual bool AuthorizeFile(string path)
+        {
+            return CanAccess(path) && IsValidFile(Path.GetExtension(path));
+        }
+    }
+}
