681e4b0f221a3ec7fd6c6afd216f2c849c8839c3
Georgi Yankov <gyankov@progress.com>
2020-06-04 16:10:45 +0300

    chore: add in memory server operations service

---
+++ graphql/graphql-aspnet-core/graphql-aspnet-core/Controllers/ServerOperationsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using graphql_aspnet_core.Data;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace graphql_aspnet_core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServerOperationsController : Controller
+    {
+        private CustomersEntitiesDataContext context;
+
+        public ServerOperationsController(CustomersEntitiesDataContext context)
+        {
+            this.context = context;
+        }
+
+        [AcceptVerbs("Get", "Post")]
+        public IActionResult Get([DataSourceRequest]DataSourceRequest request)
+        {
+            return Json(this.context.Customers.ToList().ToDataSourceResult(request));
+        }
+    }
+}
