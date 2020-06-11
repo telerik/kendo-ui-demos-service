e1f3c86d64798cca3189291a2bfa9ba38a9933c3
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2020-06-11 10:40:42 +0300

    feat(crud-service): set SameSite to None for session cookies

---
+++ demos-and-odata-v3/KendoCRUDService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(KendoCRUDService.Startup))]
+
+namespace KendoCRUDService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
