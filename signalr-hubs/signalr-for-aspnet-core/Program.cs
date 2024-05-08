5f34ca51cfae00132bae7398789c840a8c97f953
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-05-08 09:13:27 -0400

    Modernize Program.cs

--- signalr-hubs/signalr-for-aspnet-core/Program.cs
+++ signalr-hubs/signalr-for-aspnet-core/Program.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using signalr_for_aspnet_core.Hubs;
+using signalr_for_aspnet_core.Models;
 
-namespace signalr_for_aspnet_core;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateWebHostBuilder(args).Build().Run();
-    }
+builder.Services.AddCors(corsOption => corsOption.AddPolicy("CorsPolicy", corsBuilder => 
+    corsBuilder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    ));
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
-}
+builder.Services.AddDbContext<SampleEntitiesDataContext>(options => options.UseSqlite());
+
+builder.Services.AddSignalR().AddJsonProtocol(options => {
+    options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+});
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.UseRouting();
+app.MapHub<ProductHub>("/products");
+
+app.Run();
