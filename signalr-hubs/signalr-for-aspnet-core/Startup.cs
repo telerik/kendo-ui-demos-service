d50c2f9274311e2ea4db632a14ec8a4abd08416b
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-05-08 08:27:35 -0400

    Reconfigure for modern .net8 expectations/compatibility.

--- signalr-hubs/signalr-for-aspnet-core/Startup.cs
+++ signalr-hubs/signalr-for-aspnet-core/Startup.cs
@@ -2,53 +2,49 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json.Serialization;
 using signalr_for_aspnet_core.Hubs;
 
-namespace signalr_for_aspnet_core
+namespace signalr_for_aspnet_core;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration, IHostingEnvironment env)
     {
-        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        Configuration = configuration;
+        WebRootPath = env.WebRootPath;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public static string WebRootPath { get; private set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
         {
-            Configuration = configuration;
-            WebRootPath = env.WebRootPath;
-        }
+            builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowAnyOrigin();
+        }));
 
-        public IConfiguration Configuration { get; }
+        services.AddSignalR().AddJsonProtocol(options => {
+            options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-        public static string WebRootPath
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        app.UseCors("CorsPolicy");
+
+        app.UseFileServer();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
         {
-            get; private set;
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
-            {
-                builder
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowAnyOrigin()
-                    .AllowCredentials();
-            }));
-
-            services.AddSignalR()
-                .AddJsonProtocol(x =>
-                    x.PayloadSerializerSettings.ContractResolver = new DefaultContractResolver());
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseCors("CorsPolicy");
-
-            app.UseFileServer();
-
-            app.UseSignalR(routes => routes.MapHub<ProductHub>("/products"));
-        }
+            endpoints.MapHub<ProductHub>("/products");
+        });
     }
 }
