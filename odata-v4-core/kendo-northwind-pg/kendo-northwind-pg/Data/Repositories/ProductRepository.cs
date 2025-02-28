1adf7e82482f02c2b9bec653bb63d680a386999d
Angel Petrov <agpetrov@progress.com>
2025-02-28 14:29:53 +0200

    fix: resolve odata-v4 batch editing context issue

---
+++ odata-v4-core/kendo-northwind-pg/kendo-northwind-pg/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,118 @@
+﻿using kendo_northwind_pg.Data.Models;
+using kendo_northwind_pg.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace kendo_northwind_pg.Data.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ISession _session;
+        private readonly DemoDbContext _contextFactory;
+
+        public ProductRepository(IHttpContextAccessor httpContextAccessor, DemoDbContext contextFactory)
+        {
+            _session = httpContextAccessor.HttpContext.Session;
+            _contextFactory = contextFactory;
+        }
+
+        public IList<Product> All()
+        {
+            var result = _session.GetObjectFromJson<IList<Product>>("Products"); //HttpContext.Current.Session["Products"] as IList<ProductModel>;
+
+            if (result == null)
+            {
+                result =
+                    _contextFactory.Products.Select(p => new Product
+                    {
+                        ProductID = p.ProductID,
+                        ProductName = p.ProductName,
+                        UnitPrice = p.UnitPrice,
+                        UnitsInStock = p.UnitsInStock.GetValueOrDefault(),
+                        Discontinued = p.Discontinued,
+                        CategoryID = p.CategoryID.GetValueOrDefault(),
+                        UnitsOnOrder = p.UnitsOnOrder.GetValueOrDefault(),
+                        ReorderLevel = p.ReorderLevel.GetValueOrDefault(),
+                        SupplierID = p.SupplierID.GetValueOrDefault(),
+                    }).ToList();
+
+                _session.SetObjectAsJson("Products", result);
+            }
+
+            return result;
+        }
+
+        public Product One(Func<Product, bool> predicate)
+        {
+            return All().FirstOrDefault(predicate);
+        }
+
+        public IEnumerable<Product> Where(Func<Product, bool> predicate)
+        {
+            return All().Where(predicate);
+        }
+
+        public void Insert(Product product)
+        {
+            var first = All().OrderByDescending(p => p.ProductID).FirstOrDefault();
+            if (first != null)
+            {
+                product.ProductID = first.ProductID + 1;
+            }
+            else
+            {
+                product.ProductID = 0;
+            }
+
+            All().Insert(0, product);
+        }
+
+        public void Insert(IEnumerable<Product> products)
+        {
+            foreach (var product in products)
+            {
+                Insert(product);
+            }
+        }
+
+        public void Update(Product product)
+        {
+            var target = One(p => p.ProductID == product.ProductID);
+            if (target != null)
+            {
+                target.ProductName = product.ProductName;
+                target.UnitPrice = product.UnitPrice;
+                target.UnitsInStock = product.UnitsInStock;
+                target.UnitsOnOrder = product.UnitsOnOrder;
+                target.CategoryID = product.CategoryID;
+                target.SupplierID = product.SupplierID;
+                target.ReorderLevel = product.ReorderLevel;
+                target.Discontinued = product.Discontinued;
+            }
+        }
+
+        public void Update(IEnumerable<Product> products)
+        {
+            foreach (var product in products)
+            {
+                Update(product);
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            var target = One(p => p.ProductID == product.ProductID);
+            if (target != null)
+            {
+                All().Remove(target);
+            }
+        }
+
+        public void Delete(IEnumerable<Product> products)
+        {
+            foreach (var product in products)
+            {
+                Delete(product);
+            }
+        }
+    }
+}
