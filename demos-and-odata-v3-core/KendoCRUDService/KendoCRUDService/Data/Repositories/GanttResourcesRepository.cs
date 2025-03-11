425fb118f9410a3d5740c8029faaf6c34eb705c2
Angel Petrov <agpetrov@progress.com>
2025-03-11 17:46:36 +0200

    chore: fix resources controller error

--- demos-and-odata-v3-core/KendoCRUDService/KendoCRUDService/Data/Repositories/GanttResourcesRepository.cs
+++ demos-and-odata-v3-core/KendoCRUDService/KendoCRUDService/Data/Repositories/GanttResourcesRepository.cs
@@ -1,35 +1,36 @@
 ﻿using KendoCRUDService.Data.Models;
 using KendoCRUDService.SessionExtensions;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace KendoCRUDService.Data.Repositories
 {
     public class GanttResourcesRepository
     {
         private readonly ISession _session;
-        private readonly IDbContextFactory<DemoDbContext> _contextFactory;
+        private readonly IServiceScopeFactory _scopeFactory;
+        private IList<GanttResource> _resources;
 
-        public GanttResourcesRepository(IHttpContextAccessor httpContextAccessor, IDbContextFactory<DemoDbContext> contextFactory)
+        public GanttResourcesRepository(IHttpContextAccessor httpContextAccessor, IServiceScopeFactory scopeFactory)
         {
             _session = httpContextAccessor.HttpContext.Session;
-            _contextFactory = contextFactory;
+            _scopeFactory = scopeFactory;
         }
 
         public IList<GanttResource> All()
         {
             var result = _session.GetObjectFromJson<IList<GanttResource>>("GanttResources");
 
-            if (result == null)
+            if (_resources == null)
             {
-                using (var context = _contextFactory.CreateDbContext())
+                using (var scope = _scopeFactory.CreateScope())
                 {
-                    result = context.GanttResources.ToList();
+                    var context = scope.ServiceProvider.GetRequiredService<DemoDbContext>();
+                    _resources = context.GanttResources.ToList();
                 }
-
-                _session.SetObjectAsJson("GanttResources", result);
             }
 
-            return result;
+            return _resources;
         }
     }
 }
