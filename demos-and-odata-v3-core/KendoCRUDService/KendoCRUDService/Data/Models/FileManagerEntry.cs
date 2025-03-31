948c330c1301c7798ebf0672c2d379ec2702acb6
Angel Petrov <agpetrov@progress.com>
2025-03-31 16:38:46 +0300

    refactor: file services

--- demos-and-odata-v3-core/KendoCRUDService/KendoCRUDService/Data/Models/FileManagerEntry.cs
+++ demos-and-odata-v3-core/KendoCRUDService/KendoCRUDService/Data/Models/FileManagerEntry.cs
@@ -15,5 +15,23 @@
         public DateTime ModifiedUtc { get; set; }
 
         public IEnumerable<FileManagerEntry> Directories { get; set; }
+
+        public FileManagerEntry Clone()
+        {
+            return new FileManagerEntry
+            {
+                Name = this.Name,
+                Size = this.Size,
+                Path = this.Path,
+                Extension = this.Extension,
+                IsDirectory = this.IsDirectory,
+                HasDirectories = this.HasDirectories,
+                Created = this.Created,
+                CreatedUtc = this.CreatedUtc,
+                Modified = this.Modified,
+                ModifiedUtc = this.ModifiedUtc,
+                Directories = this.Directories?.Select(d => d.Clone()).ToList()
+            };
+        }
     }
 }
