4c4d6ad2775b2ed8f50975ec3ae4f01d64cb62d5
Nikolay <niko.penev@gmail.com>
2025-04-23 13:08:36 +0300

    chore: add service for for umpload demos

---
+++ demos-and-odata-v3-core/KendoCRUDService/KendoCRUDService/Models/FormUploadItemsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace KendoCRUDService.Models
+{
+    public class FormUploadItemsViewModel
+    {
+        public string TextBox { get; set; }
+        public string TextArea { get; set; }
+        public int NumericTextBox { get; set; }
+        public string MaskedTextBox { get; set; }
+        public DateTime DatePicker { get; set; }
+        public DateTime DateTimePicker { get; set; }
+        public string RadioGroup { get; set; }
+        public List<string> CheckBoxGroup { get; set; }
+        public bool Switch { get; set; }
+        public int ComboBox { get; set; }
+        public int DropDownList { get; set; }
+        public IFormFile FileName { get; set; }
+    }
+}
