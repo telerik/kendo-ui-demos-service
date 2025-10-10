8cb1585585eb69e3e2cbd865045796211b563824
Angel Petrov <agpetrov@progress.com>
2025-10-10 18:02:12 +0300

    chore(core): optimize employees repository session usage

--- demos-core/KendoCRUDService/KendoCRUDService/Data/Repositories/EmployeeRepository.cs
+++ demos-core/KendoCRUDService/KendoCRUDService/Data/Repositories/EmployeeRepository.cs
@@ -1,7 +1,8 @@
 ﻿using KendoCRUDService.Data.Models;
-using KendoCRUDService.Models;
 using KendoCRUDService.Extensions;
+using KendoCRUDService.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Concurrent;
 
 namespace KendoCRUDService.Data.Repositories
 {
@@ -10,56 +11,60 @@ namespace KendoCRUDService.Data.Repositories
     {
         private readonly ISession _session;
         private readonly IDbContextFactory<DemoDbContext> _contextFactory;
+        private readonly IServiceScopeFactory _scopeFactory;
+        private IHttpContextAccessor _contextAccessor;
+        private ConcurrentDictionary<string, IList<EmployeeViewModel>> _employees;
+        private ConcurrentDictionary<string, IList<EmployeeCompleteViewModel>> _employeesComplete;
 
-        public EmployeeRepository(IHttpContextAccessor httpContextAccessor, IDbContextFactory<DemoDbContext> contextFactory)
+        public EmployeeRepository(IHttpContextAccessor httpContextAccessor, IServiceScopeFactory scopeFactory, IDbContextFactory<DemoDbContext> contextFactory)
         {
             _session = httpContextAccessor.HttpContext.Session;
             _contextFactory = contextFactory;
+            _scopeFactory = scopeFactory;
+            _contextAccessor = httpContextAccessor;
+            _employees = new ConcurrentDictionary<string, IList<EmployeeViewModel>>();
+            _employeesComplete = new ConcurrentDictionary<string, IList<EmployeeCompleteViewModel>>();
         }
 
         public IList<EmployeeViewModel> All()
         {
+            var userKey = SessionUtils.GetUserKey(_contextAccessor);
             IList<EmployeeViewModel> result = _session.GetObjectFromJson<IList<EmployeeViewModel>>("Employees");
-
-            if (result == null)
+            return _employees.GetOrAdd(userKey, key =>
             {
-                using (var db = _contextFactory.CreateDbContext())
+                using (var scope = _scopeFactory.CreateScope())
                 {
-
-                    result = db.Employees.Select(e => new EmployeeViewModel
-                    {
-                        EmployeeId = e.EmployeeID,
-                        FullName = e.FirstName + " " + e.LastName,
-                        ReportsTo = e.ReportsTo,
-                        HasEmployees = db.Employees.Any(x=> x.ReportsTo == e.EmployeeID)
-                    }).ToList();
-
-                    _session.SetObjectAsJson("Employees", result);
+                    var context = scope.ServiceProvider.GetRequiredService<DemoDbContext>();
+                    return context.Employees.Select(e => new EmployeeViewModel
+                        {
+                            EmployeeId = e.EmployeeID,
+                            FullName = e.FirstName + " " + e.LastName,
+                            ReportsTo = e.ReportsTo,
+                            HasEmployees = context.Employees.Any(x => x.ReportsTo == e.EmployeeID)
+                        }).ToList();
                 }
-            }
-
-            return result;
+            });
         }
 
         public IList<EmployeeCompleteViewModel> AllComplete()
         {
-            IList<EmployeeCompleteViewModel> result = _session.GetObjectFromJson<IList<EmployeeCompleteViewModel>>("EmployeesComplete");
+            var userKey = SessionUtils.GetUserKey(_contextAccessor);
 
-            if (result == null)
+            return _employeesComplete.GetOrAdd(userKey, key =>
             {
-                result = _contextFactory.CreateDbContext().Employees.Select(m => new EmployeeCompleteViewModel
+                using (var scope = _scopeFactory.CreateScope())
                 {
-                    FirstName = m.FirstName,
-                    LastName = m.LastName,
-                    Country = m.Country,
-                    City = m.City,
-                    Title = m.Title
-                }).ToList();
-
-                _session.SetObjectAsJson("EmployeesComplete", result);
-            }
-
-            return result;
+                    var context = scope.ServiceProvider.GetRequiredService<DemoDbContext>();
+                    return context.Employees.Select(m => new EmployeeCompleteViewModel
+                    {
+                        FirstName = m.FirstName,
+                        LastName = m.LastName,
+                        Country = m.Country,
+                        City = m.City,
+                        Title = m.Title
+                    }).ToList();
+                }
+            });
         }
     }
 }
